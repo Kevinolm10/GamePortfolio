ed226d3f6b987d1bbc076e63d9f333ab0ba492c1
kevinolm10 <kevinolm10@gmail.com>
2025-11-07 18:37:23 +0100

    Refactor and change main scene to use phaser instead of threejs

--- client/src/scenes/MainScene.js
+++ client/src/scenes/MainScene.js
@@ -1,26 +1,19 @@
-import * as THREE from 'three';
 import Phaser from 'phaser';
 
 export default class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'MainScene' });
+  }
+
+  preload() {
+
+  }
+
   create() {
-    const canvas = this.add.dom(400, 300, document.createElement('canvas'));
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas.node });
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, 800 / 600, 0.1, 1000);
-    camera.position.z = 5;
 
-    const cube = new THREE.Mesh(
-      new THREE.BoxGeometry(),
-      new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    );
-    scene.add(cube);
+  }
 
-    const animate = () => {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    };
-    animate();
+  update() {
+
   }
 }
