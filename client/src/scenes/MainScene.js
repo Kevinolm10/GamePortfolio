ab10c97857ddd31ccd30dd636344192784c8ff60
kevinolm10 <kevinolm10@gmail.com>
2025-11-07 18:46:56 +0100

    Import assets.json and preload assets

--- client/src/scenes/MainScene.js
+++ client/src/scenes/MainScene.js
@@ -1,19 +1,22 @@
 import Phaser from 'phaser';
+import AssetLoader from '../assets/assets.json';
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: 'MainScene' });
+    this.player = null;
   }
 
-  preload() {
+  async preload() {
+    // Load all assets from JSON
+    await AssetLoader.loadAssets(this);
+  }
+
+  async create() {
 
   }
 
-  create() {
-
-  }
-
-  update() {
+  update(time, delta) {
 
   }
 }
