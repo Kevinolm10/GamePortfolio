98e640fa2b325c2936ea046c4fd765ca9d4bd4b8
kevinolm10 <kevinolm10@gmail.com>
2025-11-08 21:10:46 +0100

    Add movement to the create function for WASD, load and create the player with a camera following

--- client/src/scenes/MainScene.js
+++ client/src/scenes/MainScene.js
@@ -1,21 +1,42 @@
 import Phaser from 'phaser';
-import AssetLoader from '../assets/assets.json';
+import AssetLoader from '../components/assetLoader.js';
+import Player from '../components/player.js';
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: 'MainScene' });
-    this.player = null;
   }
 
-  async preload() {
-    await AssetLoader.loadAssets(this);
+  preload() {
+    AssetLoader.loadAssets(this);
   }
 
-  async create() {
+  create() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.wasd = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D
+    });
 
+    this.load.once('complete', () => {
+      Player.createAnimations(this);
+      this.player = new Player(this, 400, 300);
+      this.player.setScale(3);
+
+      this.cameras.main.startFollow(this.player, true, 0.08, 0.08);
+    });
   }
 
-  update(time, delta) {
+  update() {
+  if (!this.player) return;
 
+  const left = this.cursors.left.isDown || this.wasd.left.isDown;
+  const right = this.cursors.right.isDown || this.wasd.right.isDown;
+  const up = this.cursors.up.isDown || this.wasd.up.isDown;
+  const down = this.cursors.down.isDown || this.wasd.down.isDown;
+
+  this.player.move(left, right, up, down);
   }
 }
