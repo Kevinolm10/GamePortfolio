f3d5d513d8be5bda17c3e603a9b19fab555ca0ae
kevinolm10 <kevinolm10@gmail.com>
2025-11-11 19:16:49 +0100

    Change the create animations so its not called more than once, add .exist. Add physics set upp to the player file instead of the MainScene

--- client/src/components/player.js
+++ client/src/components/player.js
@@ -14,103 +14,123 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     // enable collision with canvas boundaries and other objects
     this.setCollideWorldBounds(true);
     this.body.setBounce(1, 1);
-  
+    this.body.setSize(10, 18);
+    this.body.setOffset(3, 14);
+
   }
 
   static createAnimations(scene) {
     const FRAME_RATE = 8;
+    const IDLE_FRAME_RATE = 4;
 
-    // Idle animations
-    scene.anims.create({
-      key: 'idle-down',
-      frames: [
-        { key: 'player', frame: 42 },
-        { key: 'player', frame: 43 },
-        { key: 'player', frame: 44 },
-        { key: 'player', frame: 45 },
-        { key: 'player', frame: 46 },
-        { key: 'player', frame: 47   }
-      ],
-      frameRate: 4,
-      repeat: -1
-    });
+    if (!scene.anims.exists('idle-down')) {
+      scene.anims.create({
+        key: 'idle-down',
+        frames: [
+          { key: 'player', frame: 42 },
+          { key: 'player', frame: 43 },
+          { key: 'player', frame: 44 },
+          { key: 'player', frame: 45 },
+          { key: 'player', frame: 46 },
+          { key: 'player', frame: 47 }
+        ],
+        frameRate: IDLE_FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'idle-right',
-      frames: [
-        { key: 'player', frame: 24 },
-        { key: 'player', frame: 25 },
-        { key: 'player', frame: 26 },
-        { key: 'player', frame: 27 },
-        { key: 'player', frame: 28 },
-        { key: 'player', frame: 29   }
-      ],
-      frameRate: 4,
-      repeat: -1
-    });
+    if (!scene.anims.exists('idle-right')) {
+      scene.anims.create({
+        key: 'idle-right',
+        frames: [
+          { key: 'player', frame: 24 },
+          { key: 'player', frame: 25 },
+          { key: 'player', frame: 26 },
+          { key: 'player', frame: 27 },
+          { key: 'player', frame: 28 },
+          { key: 'player', frame: 29 }
+        ],
+        frameRate: IDLE_FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'idle-left',
-      frames: [
-        { key: 'player', frame: 36 },
-        { key: 'player', frame: 37 },
-        { key: 'player', frame: 38 },
-        { key: 'player', frame: 39 },
-        { key: 'player', frame: 40 },
-        { key: 'player', frame: 41 }
-      ],
-      frameRate: 4,
-      repeat: -1
-    });
+    if (!scene.anims.exists('idle-left')) {
+      scene.anims.create({
+        key: 'idle-left',
+        frames: [
+          { key: 'player', frame: 36 },
+          { key: 'player', frame: 37 },
+          { key: 'player', frame: 38 },
+          { key: 'player', frame: 39 },
+          { key: 'player', frame: 40 },
+          { key: 'player', frame: 41 }
+        ],
+        frameRate: IDLE_FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'idle-up',
-      frames: [
-        { key: 'player', frame: 30 },
-        { key: 'player', frame: 31 },
-        { key: 'player', frame: 32 },
-        { key: 'player', frame: 33 },
-        { key: 'player', frame: 34 },
-        { key: 'player', frame: 35   }
-      ],
-      frameRate: 4,
-      repeat: -1
-    });
+    if (!scene.anims.exists('idle-up')) {
+      scene.anims.create({
+        key: 'idle-up',
+        frames: [
+          { key: 'player', frame: 30 },
+          { key: 'player', frame: 31 },
+          { key: 'player', frame: 32 },
+          { key: 'player', frame: 33 },
+          { key: 'player', frame: 34 },
+          { key: 'player', frame: 35 }
+        ],
+        frameRate: IDLE_FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'down',
-      frames: scene.anims.generateFrameNumbers('player', { start: 66, end: 70 }),
-      frameRate: FRAME_RATE,
-      repeat: -1
-    });
+    if (!scene.anims.exists('down')) {
+      scene.anims.create({
+        key: 'down',
+        frames: scene.anims.generateFrameNumbers('player', { start: 66, end: 70 }),
+        frameRate: FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'left',
-      frames: scene.anims.generateFrameNumbers('player', { start: 60, end: 65 }),
-      frameRate: FRAME_RATE,
-      repeat: -1
-    });
+    if (!scene.anims.exists('left')) {
+      scene.anims.create({
+        key: 'left',
+        frames: scene.anims.generateFrameNumbers('player', { start: 60, end: 65 }),
+        frameRate: FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'right',
-      frames: scene.anims.generateFrameNumbers('player', { start: 48, end: 53 }),
-      frameRate: FRAME_RATE,
-      repeat: -1
-    });
+    if (!scene.anims.exists('right')) {
+      scene.anims.create({
+        key: 'right',
+        frames: scene.anims.generateFrameNumbers('player', { start: 48, end: 53 }),
+        frameRate: FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'up',
-      frames: scene.anims.generateFrameNumbers('player', { start: 54, end: 59 }),
-      frameRate: FRAME_RATE,
-      repeat: -1
-    });
+    if (!scene.anims.exists('up')) {
+      scene.anims.create({
+        key: 'up',
+        frames: scene.anims.generateFrameNumbers('player', { start: 54, end: 59 }),
+        frameRate: FRAME_RATE,
+        repeat: -1
+      });
+    }
 
-    scene.anims.create({
-      key: 'phone',
-      frames: scene.anims.generateFrameNumbers('player', { start: 144, end: 152 }),
-      frameRate: FRAME_RATE,
-      repeat: -1
-    });
+    if (!scene.anims.exists('phone')) {
+      scene.anims.create({
+        key: 'phone',
+        frames: scene.anims.generateFrameNumbers('player', { start: 144, end: 152 }),
+        frameRate: FRAME_RATE,
+        repeat: -1
+      });
+    }
   }
 
   move(left, right, up, down, phone = false) {
